06d7795d7d1daf5ad1fd3aed44926b04fc354be5
Sean Macharia <Snmuanzi@gmail.com>
2026-04-19 15:32:14 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+require("datejs")
 
+function combineUsers(...args) {
+  let combinedObject = {
+    users: []
+  }
+
+  for (let arr of args) {
+    combinedObject.users = [
+      ...combinedObject.users,
+      ...arr
+    ]
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy")
+  return combinedObject
+}
 
 
 module.exports = {
